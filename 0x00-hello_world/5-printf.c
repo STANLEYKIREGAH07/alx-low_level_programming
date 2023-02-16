2bf6fb10607ccc537a440c1adc8cf839cc5c19e8
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-02-16 05:06:24 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdi.h>
+/**
+ * main - a progrom using the printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
