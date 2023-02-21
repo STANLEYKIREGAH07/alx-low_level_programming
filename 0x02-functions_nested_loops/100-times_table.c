d2f769515509fc32604c3e3eb2792d5969bab95e
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-02-21 10:19:33 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,10 +27,13 @@ if (prod >= 100)
 _putchar((prod / 100) + '0');
 _putchar(((prod / 10)) % 10 + '0');
 }
+else if (prod >= 10 && prod <= 99)
+{
 _putchar((prod / 10) + '0');
 }
 _putchar((prod % 10) + '0');
 }
 _putchar('\n');
+}
 }
 }
