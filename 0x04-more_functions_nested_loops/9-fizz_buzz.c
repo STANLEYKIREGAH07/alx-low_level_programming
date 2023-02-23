5d96b42ced8e4fed79f80adc50b474d0d12570b9
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-02-23 14:20:01 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,30 +10,30 @@ int main(void)
 {
 int i;
 
-for (i = 1; i <= 100; i++)i
+for (i = 1; i <= 100; i++)
 {
-	if (i % 3 == 0 && i % 5 != 0)
-	{
-		printf(" Fizz");
-	}
-	else if (i % 5 == 0 && i % 3 != 0)
-	{
-		printf(" Buzz");
-	}
-	else if (i % 3 == 0 && i % 5 == 0)
-	{
-		printf(" FizzBuzz");
-	}
-	else if (i == 1)
-	{
-		printf("%d", i);
-	}
-	else
-	{
-		printf(" %d", i);
-	}
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz");
 }
-printf("\n");
-return (0);
+else if (i % 3 == 0)
+{
+printf("Fizz");
 }
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("\d", 1);
+}
+if (i != 100)
+{
+printf(" ");
+}
+}
+printf("\n"
+	return (0);
+	}
 
