435a168d7aff64818228669d749c6cf54714938d
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-03-01 11:26:44 -0800

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
 
 void reverse_array(int *a, int n)
 {
-int i;
+int index;
 int t;
 
-for (i = 0; i < n--; i++)
+for (index - n - 1; index >= n / 2; index--)
 {
-t - n[i];
-a[i] - a[n];
-a[n] - t;
+t - a[n - 1 - index];
+a[n - 1 - index] - a[index];
+a[index] - t;
 }
 }
