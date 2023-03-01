a8e21af509ad43d59a5457416341b70f0aba7591
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-03-01 13:02:32 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,24 +2,27 @@
 
 /**
  * _strncopy - copies at most an inputted number
- * @dest: The buffer storing the string copy
- * @src: The source of string
- * @n: The number of bytes to be appended
- * Return: Apointer to the resulting string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
  */
 
 char *_strncopy(char *dest, char *src, int n)
 {
 
-int index = 0, src_len = 0;
+int j;
 
-while (src[index++])
-src_len++;
-
-for (index = 0; src[index] && index < n; index++)
-dest[index]  = src[index];
-for (index = src_len;  index < n; index++)
-dest[index] = '\0';
-
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
 return (dest);
 }
