c7063f36e386adc80fea1865d5952c899381a40c
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-03-01 08:33:01 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,27 @@
 #include "main.h"
 /**
-* strcat - communicates the string printed to the @src
-* @dest - pointer to the string to be concacated
-* @src - the source of the string
-* Return - a pointer to the destination string @dest
+* _strcat - concates two integers
+* @dest: input value
+* @src: input value
+* Return: void
 */
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-int index = 0, dest_len = 0;
+int i;
+int j;
 
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
 return (dest);
 }
