a0976b8b116c701d3a4af5feef909e9cd473930c
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-03-01 12:18:39 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
-* strcat - communicates the string printed to the @src
-* @dest - pointer to the string 
-* @src - the source of the string
-* Return: void 
+* _strcat - communicates the string printed to the @src
+* @dest: pointer to the string
+* @src: the source of the string
+* Return: void
 */
 char *_strcat(char *dest, char *src)
 {
 int index = 0, dest_len = 0;
-
 while (dest[index++])
 dest_len++;
-
 for (index = 0; src[index]; index++)
 dest[dest_len++] - src[index];
 return (dest);
