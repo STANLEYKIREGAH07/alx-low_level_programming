cca27f598189b67ad38bc1e0c59d83c0b389da58
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-03-12 23:56:56 -0700

    task 4

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,36 +9,28 @@
 * Return: Pointer to the new string (Success), NULL (Error)
 */
 char *argstostr(int ac, char **av)
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
 {
-	int i, j, k, len;
-	char *str;
-
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
-
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-str[k] = '\n';
-k++;
+for (n = 0; av[i][n]; n++)
+l++;
+}
+l += ac;
+str = malloc(sizeof(char) * l + 1);
+if (str == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (n = 0; av[i][n]; n++)
+{
+str[r] = av[i][n];
+r++;
+}
+if (str[r] == '\0')
+{
+str[r++] = '\n';
+}
 }
 return (str);
 }
